fd6a377be32ce825450bcc99d8b79eebe3140cbb
Danielgdhsdfghg <dgasdf@gmail.com>
2019-06-23 12:26:54 -0300

    Tabela venda adicionada

---
+++ SoftwareCrediario/src/main/java/modelo/Venda.java
@@ -0,0 +1,82 @@
+package modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "venda")
+public class Venda implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_venda")
+    private int id;
+
+     @ManyToMany(mappedBy = "vendas")
+    private List<Vendedor> vendedores = new ArrayList<>();
+    
+    
+    @Column(name = "nome_venda")
+    private String nome;
+
+    @Column(name = "valor")
+    private Double valor;
+
+    @Column(name = "num_parcelas")
+    private int numParcelas;
+
+    @Column(name = "valor_parcelas")
+    private Double valorParcelas;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Vendedor> getVendedores() {
+        return vendedores;
+    }
+
+    public void setVendedores(List<Vendedor> vendedores) {
+        this.vendedores = vendedores;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public int getNumParcelas() {
+        return numParcelas;
+    }
+
+    public void setNumParcelas(int numParcelas) {
+        this.numParcelas = numParcelas;
+    }
+
+    public Double getValorParcelas() {
+        return valorParcelas;
+    }
+
+    public void setValorParcelas(Double valorParcelas) {
+        this.valorParcelas = valorParcelas;
+    }
+
+    
+}
--- SoftwareCrediario/src/main/java/modelo/Vendedor.java
+++ SoftwareCrediario/src/main/java/modelo/Vendedor.java
@@ -1,6 +1,8 @@
 package modelo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.*;
 
 @Entity
@@ -12,22 +14,39 @@ public class Vendedor implements Serializable {
     @Column(name = "id_Vendedor")
     private int id;
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "vendedores_vendas", joinColumns = {
+        @JoinColumn(name = "vendedor_id",
+                referencedColumnName = "id_vendedor")}, inverseJoinColumns = {
+        @JoinColumn(name = "venda_id")})
+    private List<Venda> vendas = new ArrayList<>();
+
+    public void addVenda(Venda nVenda) {
+        vendas.add(nVenda);
+        nVenda.getVendedores().add(this);
+    }
+
+    public void removeVenda(Venda nVenda) {
+        vendas.remove(nVenda);
+        nVenda.getVendedores().remove(this);
+    }
+
     @Column(name = "cpf")
     private String cpf;
-    
+
     @Column(name = "nome")
     private String nome;
-
-    
-    //@OneToOne
-    /*(cascade = {CascadeType.ALL}, orphanRemoval = true)*/
-    //@JoinColumn(name = "credencial_id")
-    //private Credencial credencial;
 
     public Vendedor() {
     }
 
+    public List<Venda> getVendas() {
+        return vendas;
+    }
 
+    public void setVendas(List<Venda> vendas) {
+        this.vendas = vendas;
+    }
 
     public String getNome() {
         return nome;
@@ -36,7 +55,6 @@ public class Vendedor implements Serializable {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
 
     public int getId() {
         return id;
